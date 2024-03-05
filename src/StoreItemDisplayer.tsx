1cd400383e9f945afe8a47913ac02440951638d0
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-05 10:07:57 +0100

    Mise à jour de l'affichage de l'image du produit

--- src/StoreItemDisplayer.tsx
+++ src/StoreItemDisplayer.tsx
@@ -15,7 +15,7 @@ const StoreItemDisplayer: React.FC<StoreItemDisplayerProps> = ({ item, onAddToBa
                 item.discount !== undefined ? <p className="discount">{(item.price * (1 - item.discount)).toFixed(2) + " €"}</p> : <p>{item.price.toFixed(2) + " €"}</p>
             }
             {item.discount !== undefined ? <div>Discount!</div> : <div>Normal price!</div>}
-            {item.picture !== undefined ? <img className="storeItemPicture" src={item.picture} alt="Beautiful picture of the item" /> : <div className="storeItemPicture">No picture available</div>}
+            {item.picture !== undefined ? <img className="storeItemPicture" src={item.picture} alt={item.name} /> : <div className="storeItemPicture">No picture available</div>}
             <button onClick={() => onAddToBasket(item)}>Ajouter au panier</button>
         </div>
     );
