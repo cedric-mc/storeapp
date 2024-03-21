20a6c153cbc0cd45c9613520e06278286a739418
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-21 09:01:35 +0100

    Supprimer les importations inutiles dans ChatMessageDisplayer.jsx

--- src/ChatMessageDisplayer.tsx
+++ src/ChatMessageDisplayer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { ChatLauncher } from "./ChatLauncher";
-import { ChatDisplayer } from "./ChatDisplayer";
-import { ChatMessage } from "./ChatMessage";
 
 const ChatMessageDisplayer = (props: { messages: { name: string, message: string }[], onMessageSent: (message: string) => void }) => {
     return (
         <div>
+
         </div>
     );
 };
