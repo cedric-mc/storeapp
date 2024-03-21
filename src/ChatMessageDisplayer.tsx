52ceefc0000d613f1dda74629179404e005c089f
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-21 09:07:13 +0100

    Ajouter un style pour afficher les messages dans le chat

--- src/ChatMessageDisplayer.tsx
+++ src/ChatMessageDisplayer.tsx
@@ -3,7 +3,7 @@ import "./style/ChatMessageDisplayer.css"
 
 const ChatMessageDisplayer = (props: { messages: { name: string, message: string }[], onMessageSent: (message: string) => void }) => {
     return (
-        <div>
+        <div className="chat-message">
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
