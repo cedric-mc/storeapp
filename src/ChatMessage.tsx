b577148cdd182b67acd2c787a694aab2f1bb2c4f
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-21 11:53:34 +0100

    Mise à jour de ChatMessage.tsx et des styles CSS

--- src/ChatMessage.tsx
+++ src/ChatMessage.tsx
@@ -6,6 +6,20 @@ interface Message {
     date: Date
 }
 
-const ChatMessage = (props: { message: Message }) => { }
+const ChatMessage = (props: { message: Message }) => {
+    return (
+        <div>
+            <p>
+                {props.message.content}
+            </p>
+            <button onClick={() => {
+                navigator.clipboard.writeText(props.message.content).then(r => {
+                    console.log('copied')
+                }
+                )
+            }}>Copier</button>
+        </div>
+    );
+}
 
 export default ChatMessage;
