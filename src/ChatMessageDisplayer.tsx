fde4a1f789e49d2ab6ec93dd7fd6314ae4885fc7
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-21 08:52:00 +0100

    Mise à jour du composant ChatMessageDisplayer

--- src/ChatMessageDisplayer.tsx
+++ src/ChatMessageDisplayer.tsx
@@ -5,8 +5,8 @@ import { ChatDisplayer } from "./ChatDisplayer";
 const ChatMessageDisplayer = (props: { messages: { name: string, message: string }[], onMessageSent: (message: string) => void }) => {
     return (
         <div>
-            {props.messages.map((message, index) => <div key={index}>{message.name}: {message.message}</div>)}
-            <ChatDisplayer onMessageSent={props.onMessageSent} />
         </div>
     );
-}
+};
+
+export default ChatMessageDisplayer;
